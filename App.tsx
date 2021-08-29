aea18b39d7a545ca9cee712365f227d43210f009
alternacrow <k.horita21+github@gmail.com>
2021-08-29 19:59:08 +0900

    Chore expo-notificationsの追加

--- App.tsx
+++ App.tsx
@@ -1,117 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {VFC, useState, useEffect} from 'react';
 import {
+  StyleSheet,
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  Button,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  requestPermissionsAsync,
+  getPermissionsAsync,
+  scheduleNotificationAsync,
+  getAllScheduledNotificationsAsync,
+  NotificationPermissionsStatus,
+  NotificationRequest,
+} from 'expo-notifications';
 
-import {Constants} from 'react-native-unimodules';
-console.log(Constants.systemFonts);
+const App: VFC = () => {
+  const [notificationPermissionStatus, setNotificationPermissionsStatus] =
+    useState<NotificationPermissionsStatus>();
+  const [notificationRequestList, setNotificationRequestList] = useState<
+    NotificationRequest[]
+  >([]);
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handlePressRequestPermissions = async () => {
+    const status = await requestPermissionsAsync();
+    setNotificationPermissionsStatus(status);
   };
 
+  const handlePressScheduleNotification = async () => {
+    await scheduleNotificationAsync({
+      content: {
+        title: 'title',
+        body: 'body',
+      },
+      trigger: {
+        seconds: 10, // 10秒後
+      },
+    });
+
+    const list = await getAllScheduledNotificationsAsync();
+    setNotificationRequestList(list);
+  };
+
+  const handlePressGetSchedules = async () => {
+    const list = await getAllScheduledNotificationsAsync();
+    setNotificationRequestList(list);
+  };
+
+  useEffect(() => {
+    getPermissionsAsync().then(status => {
+      setNotificationPermissionsStatus(status);
+    });
+    getAllScheduledNotificationsAsync().then(list => {
+      setNotificationRequestList(list);
+    });
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <SafeAreaView style={styles.safe_area_view}>
+        <Button
+          title="Request Permissions"
+          onPress={handlePressRequestPermissions}
+        />
+        <Button
+          title="Schedule Notification"
+          onPress={handlePressScheduleNotification}
+        />
+        <Button title="Get Schedules" onPress={handlePressGetSchedules} />
+
+        <View style={styles.section}>
+          <Text style={styles.section_title}>
+            Notificatoin Permission Status
+          </Text>
+          <View style={styles.info_box}>
+            <Text>{JSON.stringify(notificationPermissionStatus, null, 2)}</Text>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        <View style={styles.section}>
+          <Text style={styles.section_title}>Notification Request List</Text>
+          <View style={styles.info_box}>
+            <Text>
+              {notificationRequestList.length > 0
+                ? JSON.stringify(notificationRequestList, null, 2)
+                : 'None'}
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  safe_area_view: {
+    flex: 1,
+    paddingVertical: 32,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  section: {
+    marginTop: 16,
+    alignItems: 'center',
   },
-  sectionDescription: {
+  section_title: {
+    fontSize: 16,
+  },
+  info_box: {
     marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    width: '80%',
+    padding: 8,
   },
 });
 
